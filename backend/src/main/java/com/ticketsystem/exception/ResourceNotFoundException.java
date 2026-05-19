672eaf6330a47e1689cfe4b5b67c2d28801186ef
ArunKumar0707 <115449190+ArunKumar0707@users.noreply.github.com>
2026-05-19 18:44:03 +0530

    Changed Backend file

--- backend/src/main/java/com/ticketsystem/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ticketsystem.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
